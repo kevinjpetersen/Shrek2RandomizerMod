e36e06d0a9c07afb2ccf5516b348576aa426f16b
Kevin J. Petersen <kevingeeken@gmail.com>
2020-05-02 02:16:50 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Shrek2RandomizerMod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cmdLines = new List<string>();
+
+            // Add Reset on Characters
+            foreach (var character in Shrek2Actors.Characters)
+                cmdLines.Add(GenerateSkeletalMeshLine(character, character));
+
+            // Affected Characters
+            Shrek2Actors.Characters.Shuffle();
+            var affectedCharacters = Shrek2Actors.Characters.ToList();
+
+            // Assigned Characters
+            Shrek2Actors.Characters.Shuffle();
+            var assignedCharacters = Shrek2Actors.Characters.ToList();
+
+            for(int i = 0; i < affectedCharacters.Count; i++)
+            {
+                var afc = affectedCharacters[i];
+                var asc = assignedCharacters[i];
+
+                cmdLines.Add(GenerateSkeletalMeshLine(afc, asc));
+            }
+
+            File.WriteAllLines(Path.Combine(Directory.GetCurrentDirectory(), "SH2Random"), cmdLines);
+        }
+
+        static string GenerateSkeletalMeshLine(string affectedCharacter, string assignedCharacter) =>
+            $"set {affectedCharacter} mesh skeletalmesh'ShrekCharacters.{assignedCharacter}'";
+    }
+}
---
+++ Shrek2Actors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shrek2RandomizerMod
+{
+    public class Shrek2Actors
+    {
+        public static List<string> Characters = new List<string>()
+        {
+            "Shrek",
+            "HumanShrek",
+            "HazMatShrek",
+            "Donkey",
+            "DonkeyChair",
+            "Steed",
+            "PIB",
+            "King",
+            "Frog",
+            "FGM",
+            "GBMan",
+            "Mongo",
+            "Peasant",
+            "PeasantWoman",
+            "Girl",
+            "Boy",
+            "Spider",
+            "Chicken",
+            "Bat",
+            "Bandit",
+            "Wheelbandit",
+            "Fiona_O",
+            "Fiona_H",
+            "Torch",
+            "Rat",
+            "Mouse",
+            "Cow",
+            "Knight",
+            "FatKnight",
+            "Prisoner",
+            "ElfHazmat"
+        };
+    }
+}
